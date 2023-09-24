bafa9a0687dcc36ca34411a36af22d402568aac9
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-24 20:19:27 +0300

    qweqwe

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,34 +1,56 @@
 
-const refs = {
-  form: document.querySelector('.form'),
-  delay: document.querySelector('input[name="delay"]'),
-  step: document.querySelector('input[name="step"]'),
-  amount: document.querySelector('input[name="amount"]'),
-  submitBtn: document.querySelector('button'),
-}
-
-console.log(refs);
 
 
+let firstDelay = [];
+let delayStep = {};
+let amount = {};
+
+
+// const refs = {
+//   form: document.querySelector('.form'),
+//   //delay: document.querySelector('input[name="delay"]'),
+//   //step: document.querySelector('input[name="step"]'),
+//   //amount: document.querySelector('input[name="amount"]'),
+//   submitBtn: document.querySelector('button'),
+// }
+
+const form = document.querySelector('.form');
+
+console.log(form);
+
+// const array = [...form.children].map(el => el);
+// console.log(array);
+firstDelay[form] = form.lastElementChild;
+console.log(firstDelay);
+
+// console.log(array);
 
 
 
-refs.submitBtn.addEventListener('submit', (evt) => {
-  evt.preventDefault();
- 
-})
+
+
+  
 
 
 
 
 
 
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }

c1f3474afe30ba4a2c121b202b2bc5dc6210b497
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-25 15:34:56 +0000

    Deploying to gh-pages from @ NikVio/goit-js-hw-09@abc765d85f7cd68d8040597b7a1f79a887a7d2fe 🚀

--- 03-promises.5b9e5e46.js
+++
@@ -1,2 +0,0 @@
-let o=[];const e=document.querySelector(".form");console.log(e),o[e]=e.lastElementChild,console.log(o);
-//# sourceMappingURL=03-promises.5b9e5e46.js.map
--- 03-promises.ac2780db.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=[],e=document.querySelector(".form");console.log(e),o[e]=e.lastElementChild,console.log(o)}();
-//# sourceMappingURL=03-promises.ac2780db.js.map
